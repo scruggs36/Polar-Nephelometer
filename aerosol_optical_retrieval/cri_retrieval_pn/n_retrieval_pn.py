0ec08de81ca88b7394455e04eca4315c4b10000e
scruggs36 <scruggs36mobile@gmail.com>
2019-07-30 21:35:02 -0400

    Retrieval working for wide window of CRI case! Search for minimum is brute force, not intelligent!

--- aerosol_optical_retrieval/cri_retrieval_pn/n_retrieval_pn.py
+++ aerosol_optical_retrieval/cri_retrieval_pn/n_retrieval_pn.py
@@ -8,10 +8,150 @@ import PyMieScatt as ps
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from math import log, sqrt, pi
+import matplotlib.cm as cm
+import os
 from scipy.stats import chisquare
-path = '/home/austen/Desktop/'
+import datetime
+from math import log, sqrt, pi
+
+
 # generate artificial data, this is the creation of the log normal distribution
+
+# write function for LogNormal distribution (same as Tami Bonds)
+def LogNormal(size, mu, gsd, N):
+    #return (N / (sqrt(2 * pi) * log(gsd))) * np.exp(-1 * ((log(size) - log(mu)) ** 2) / (2 * log(gsd) ** 2))
+    return (N / (sqrt(2 * pi) * size * log(gsd))) * np.exp(-1 * ((log(size) - log(mu)) ** 2) / (2 * log(gsd) ** 2))
+
+
+def Retrieval_v0(path, number_density, geom_stdev, diameter, wavelength, m_known,  added_noise_array, n_space, k_space):
+    pf_2darray = []
+    pf_2darray_R = []
+    pf_arr = []
+    theta_2darray = []
+    m_R_space = []
+    n_R_space = []
+    k_R_space = []
+    pf_space = []
+    chi_square_space = []
+    chi_arr = []
+    # create distribution data
+    sizes = np.arange(700, 1110, 10)
+    log_dist = [LogNormal(element, diameter, geom_stdev, number_density) for element in sizes]
+
+    # plot distribution
+    f0, ax0 = plt.subplots(figsize=(12, 12))
+    ax0.plot(sizes, log_dist, 'r-', label='lognormal dist.: \u03bc=' + str(d) + ' $\u03c3_{g}$=' + str(sigma_g))
+    ax0.set_xlabel('particle diameter (nm)')
+    ax0.set_ylabel('dN/dD')
+    ax0.set_title('Log Normal Distribution')
+    ax0.grid(True)
+    ax0.legend(loc=1)
+    plt.savefig(path + 'size_distribution.pdf', format='pdf')
+    #plt.show()
+
+    for s in added_noise_array:
+        for counter, element in enumerate(sizes):
+            theta, SL, SR, SU = ps.ScatteringFunction(m_known, wavelength, element, nMedium=1.0, minAngle=0, maxAngle=180, angularResolution=0.5, space='theta', angleMeasure='degrees', normalization=None)
+            # creating noise in phase functions
+            noise_mu = np.mean(0)
+            noise_sigma = s
+            noise = np.random.normal(noise_mu, noise_sigma, len(SU))
+            # Noise up the original signal
+            SU_noise = SU + noise
+            pf_2darray.append(SU_noise)
+
+        # calculate weighted average of phase function data based on weights determined from the lognormal distribution
+        pf_average = np.average(pf_2darray, axis=0, weights=log_dist)
+
+        # plot phase functions that comprise the weighted average phase function
+        f1, ax1 = plt.subplots(1, 2, figsize=(12, 12))
+        for counter, element in enumerate(pf_2darray):
+            ax1[0].semilogy(theta, element, label='P.F. @ size: ' + str(sizes[counter]))
+        ax1[0].legend(loc=1, ncol=3)
+        ax1[0].set_title('Phase Functions Used to Create Weighted Average Phase Function')
+        ax1[0].set_xlabel('\u03b8 (\u0b00)')
+        ax1[0].set_ylabel('Intensity')
+        ax1[0].grid(True)
+        ax1[1].semilogy(theta, pf_average, label='Weighted Average Phase Function')
+        ax1[1].legend(loc=1)
+        ax1[1].set_title('Weighted Average Phase Function')
+        ax1[1].set_xlabel('\u03b8 (\u00b0)')
+        ax1[1].set_ylabel('Intensity')
+        ax1[1].grid(True)
+        plt.savefig(path + 'generated_pfs.pdf', format='pdf')
+        #plt.show()
+
+        # i am right here, just changed names to n_space and k_space
+        for n in n_space:
+            for k in k_space:
+                m_R = complex(n, k)
+                print(m_R)
+                m_R_space.append(m_R)
+                n_R_space.append(n)
+                k_R_space.append(k)
+                for element in sizes:
+                    theta_R, SL_R, SR_R, SU_R = ps.ScatteringFunction(m_R, w_n, element, nMedium=1.0, minAngle=0, maxAngle=180, angularResolution=0.5, space='theta', angleMeasure='degrees', normalization=None)
+                    pf_2darray_R.append(SU_R)
+                pf_average_R = np.average(pf_2darray_R, axis=0, weights=log_dist)
+                # the std will be different when using real data, for now we set it to the difference between data and model at each point
+                std = np.abs(pf_average - pf_average_R)
+                # chi square = residual divided by standard deviation at each angle, we call it chi square as the is value has a chi square distribution, but it is not a chi square statistic
+                chisq = np.sum((pf_average - pf_average_R)**2 / std)
+                theta_2darray.append(theta_R)
+                pf_arr.append(pf_average_R)
+                chi_arr.append(chisq)
+                pf_2darray_R = []
+            chi_square_space.append(chi_arr)
+            pf_space.append(pf_arr)
+            chi_arr = []
+
+        chi_square_space = np.transpose(chi_square_space)
+        k_idx_min, n_idx_min = np.where(chi_square_space == np.min(chi_square_space))
+        k_idx_min = int(k_idx_min)
+        n_idx_min = int(n_idx_min)
+        #print(np.array(chi_square_space).shape)
+        X, Y = np.meshgrid(n_space, k_space)
+        f2, ax2 = plt.subplots(1, 2, figsize=(12, 12))
+        #print(X.shape)
+        #print(X)
+        ax2[0].set_title('Contour Chi Square Minimization')
+        c1 = ax2[0].pcolormesh(X, Y, chi_square_space, cmap='rainbow')
+        ax2[0].set_xlabel('n')
+        ax2[0].set_ylabel('k')
+        f2.colorbar(c1, ax=ax2[0])
+        ax2[0].plot(n_space[n_idx_min], k_space[k_idx_min], color='black', marker='X', ms=16)
+        ax2[1].semilogy(theta_R, pf_average, color='black', ls='-', label='Input Phase Function')
+        ax2[1].semilogy(theta_R, pf_space[n_idx_min][k_idx_min], color='red', ls='--', label='Retrieved Phase Function')
+        ax2[1].set_title('Input and Retrieved Phase Functions')
+        ax2[1].set_xlabel('\u03b8 (\u00b0)')
+        ax2[1].set_ylabel('Intensity')
+        ax2[1].legend(loc=1)
+        ax2[1].grid(True)
+        plt.savefig(path + 'generated_pfs.pdf', format='pdf')
+
+        X_array = []
+        Y_array = []
+        chi_square_array = []
+        df1 = pd.DataFrame()
+        for element in X:
+            X_array.append(element)
+        df1['n space'] = X_array
+        for element in Y:
+            Y_array.append(element)
+        df1['k space'] = Y_array
+        for element in chi_square_space:
+            chi_square_array.append(element)
+        df1['Chi Square Statistic'] = chi_square_array
+        df1.to_csv(path + 'CHI_DATA_NOISE_@' + str(s) + '.txt')
+        df2 = pd.DataFrame()
+        df2['Theta'] = theta_R
+        df2['Input Phase Function'] = pf_average
+        df2.to_csv(path + 'PF_INPUT_NOISE_@' + str(s) + '.txt')
+        #np.savetxt(path + 'PF_SPACE_@' + str(s) + '.txt', pf_space, delimiter=',')
+
+
+
+
 # total particle concentration
 concentration = 1000
 # geometric standard deviation
@@ -22,109 +162,24 @@ d= 900
 w_n = 663
 # CRI
 m = 1.59 + 0.0j
-# write function for LogNormal distribution (same as Tami Bonds)
-def LogNormal(size, mu, gsd, N):
-    #return (N / (sqrt(2 * pi) * log(gsd))) * np.exp(-1 * ((log(size) - log(mu)) ** 2) / (2 * log(gsd) ** 2))
-    return (N / (sqrt(2 * pi) * size * log(gsd))) * np.exp(-1 * ((log(size) - log(mu)) ** 2) / (2 * log(gsd) ** 2))
-
-# create distribution data
-sizes = np.arange(700, 1110, 10)
-log_dist = [LogNormal(element, d, sigma_g, concentration) for element in sizes]
-
-# plot distribution
-f0, ax0 = plt.subplots(figsize=(12, 12))
-ax0.plot(sizes, log_dist, 'r-', label='lognormal dist.: \u03bc=' + str(d) + ' $\u03c3_{g}$=' + str(sigma_g))
-ax0.set_xlabel('particle diameter (nm)')
-ax0.set_ylabel('dN/dD')
-ax0.set_title('Log Normal Distribution')
-ax0.grid(True)
-ax0.legend(loc=1)
-#plt.savefig(Save_Directory + 'Mie_Distributions.pdf', format='pdf')
-plt.show()
-
 # generate phase function data
-pf_2darray = []
-
-for counter, element in enumerate(sizes):
-    theta, SL, SR, SU = ps.ScatteringFunction(m, w_n, element, nMedium=1.0, minAngle=0, maxAngle=180, angularResolution=0.5, space='theta', angleMeasure='degrees', normalization=None)
-    # creating noise in phase functions
-    noise_mu = np.mean(SU)
-    noise_sigma = 1
-    noise = np.random.normal(noise_mu, noise_sigma, len(SU))
-    # Noise up the original signal
-    SU_noise = SU + noise
-    pf_2darray.append(SU_noise)
-
-# plot phase functions that comprise the weighted average phase function
-f1, ax1 = plt.subplots(figsize=(12, 12))
-for counter, element in enumerate(pf_2darray):
-    ax1.semilogy(theta, element, label='P.F. @ size: ' + str(sizes[counter]))
-ax1.legend(loc=1, ncol=3)
-ax1.set_title('Phase Functions Used to Create Weighted Average Phase Function')
-ax1.set_xlabel('\u03b8 (\u0b00)')
-ax1.set_ylabel('Intensity')
-ax1.grid(True)
-plt.show()
-
-# calculate weighted average of phase function data based on weights determined from the lognormal distribution
-pf_average = np.average(pf_2darray, axis=0, weights=log_dist)
-
-# plot weighted average phase function
-f2, ax2 = plt.subplots(figsize=(12,12))
-ax2.semilogy(theta, pf_average, label='Weighted Average Phase Function')
-ax2.legend(loc=1)
-ax2.set_title('Weighted Average Phase Function')
-ax2.set_xlabel('\u03b8 (\u00b0)')
-ax2.set_ylabel('Intensity')
-ax2.grid(True)
-plt.show()
-
+#noise_sigma_array = np.arange(1.0, 2.0, 0.2)
+noise_sigma_array = [0.1]
 # testing retrieval with data generated from mie theory with added noise
-cri_n_space = np.arange(1.54, 1.59, .01)
-cri_k_space = np.arange(0.0, 0.1, 0.02)
-pf_2darray_R = []
-m_R_space = []
-pf_space = []
-chi_square_space = []
-p_val_space = []
-for n in cri_n_space:
-    for k in cri_k_space:
-        m_R = complex(n, k)
-        print(m_R)
-        m_R_space.append(m_R)
-        for element in sizes:
-            theta_R, SL_R, SR_R, SU_R = ps.ScatteringFunction(m_R, w_n, element, nMedium=1.0, minAngle=0, maxAngle=180, angularResolution=0.5, space='theta', angleMeasure='degrees', normalization=None)
-            pf_2darray_R.append(SU_R)
-        pf_average_R = np.average(pf_2darray_R, axis=0, weights=log_dist)
-        pf_2darray_R = []
-        pf_space.append(pf_average_R)
-        chi_val, p_val = chisquare(f_obs=pf_average, f_exp=pf_average_R)
-        chi_square_space.append(chi_val)
-        p_val_space.append(p_val)
+cri_n_space = np.arange(1.5, 1.7, .01)
+#print(len(cri_n_space))
+cri_k_space = np.arange(-0.1, 0.1, 0.01)
+#print(len(cri_k_space))
 
-'''
-df1 = pd.DataFrame()
-df['CRI'] = m_R_space
-df1['Chi Square Statistic'] = chi_square_space
-df1['P Val'] = p_val_space
-df2['Theta'] = theta_R
-df2['Phase Function Space'] = pf_space
-df1.to_csv(path + 'data.txt')
-'''
+for counter, sigma in enumerate(noise_sigma_array):
+    now = datetime.datetime.now()
+    data_path = 'Data/Added_Noise/' + str(now.year) + '_' + str(now.month) + '_' + str(now.day) + '_' + str(now.hour) + '_' + str(now.minute) + '_' + str(now.second) + '_' + 'sigma_' + str(sigma) + '/'
+    try:
+        os.makedirs(os.path.expanduser(data_path))
+        Retrieval_v0(path=data_path, number_density=concentration, geom_stdev=sigma_g, diameter=d, wavelength=w_n, m_known=m,  added_noise_array=noise_sigma_array, n_space=cri_n_space, k_space=cri_k_space)
+    except FileExistsError:
+        print('File already exists')
 
-f3, ax3 = plt.subplots(1, 2, figsize=(12, 12))
-for counter, element in enumerate(pf_space):
-    ax3[0].plot(theta_R, element, label='PF @ m = ' + str(m_R_space[counter]))
-ax3[0].set_title('Phase Functions at Various Refractive Indices')
-ax3[0].set_xlabel('\u03b8 (\u0b00)')
-ax3[0].set_ylabel('Intensity')
-ax3[0].legend(loc=1)
-ax3[0].grid(True)
-ax3[1].plot(m_R_space, chi_square_space, color='black', marker='o', ls=' ', label='Chi stat. vs. m')
-ax3[1].set_xlabel('Complex Refractive Index')
-ax3[1].set_ylabel('Chi Square Stat.')
-ax3[1].legend(loc=1)
-ax3[1].grid(True)
-plt.show()
+
 
 
